82e4a5f10ae3c1e58b2a47c4a6f56ce9344f338a
a2z-dev007 <azdev776@gmail.com>
2026-02-17 00:19:55 +0530

    Images path updated

--- frontend/src/constants/assets.ts
+++ frontend/src/constants/assets.ts
@@ -2,14 +2,14 @@ export const ASSETS = {
     HERO: {
         BG: '/assets/hero-bg.png',
         BAG: '/assets/bag.png',
-        BACKGROUND: '/assets/hero/Background Image.png',
-        FOREGROUND: '/assets/hero/Foreground Image.png',
-        PRODUCT: '/assets/hero/Product Image.png',
+        BACKGROUND: '/assets/hero/Background-Image.png',
+        FOREGROUND: '/assets/hero/Foreground-Image.png',
+        PRODUCT: '/assets/hero/Product-Image.png',
     },
     FRONT_BACK_SIDEWAYS: {
-        IMAGE_1: '/assets/front-back-sideways/image 3.png',
-        IMAGE_2: '/assets/front-back-sideways/image 2.png',
-        IMAGE_3: '/assets/front-back-sideways/image 1.png',
+        IMAGE_1: '/assets/front-back-sideways/image-3.png',
+        IMAGE_2: '/assets/front-back-sideways/image-2.png',
+        IMAGE_3: '/assets/front-back-sideways/image-1.png',
     },
     TICKERS: {
         MAIN: '/assets/tickers/main.jpeg',
@@ -24,17 +24,17 @@ export const ASSETS = {
         IMG_354A7751: '/assets/tickers/354A7751.jpg',
     },
     ABOUT: {
-        FEATURE: '/assets/about/Feature Image.png',
-        IMG_1ST: '/assets/about/1st Image.png',
-        IMG_2ND: '/assets/about/2nd Image.png',
-        IMG_3RD: '/assets/about/3rd Image.png',
+        FEATURE: '/assets/about/Feature-Image.png',
+        IMG_1ST: '/assets/about/1st-Image.png',
+        IMG_2ND: '/assets/about/2nd-Image.png',
+        IMG_3RD: '/assets/about/3rd-Image.png',
     },
     BENEFITS: {
-        CARD_1ST: '/assets/benefits/1st card.png',
-        CARD_2ND: '/assets/benefits/2nd card.png',
-        CARD_3RD: '/assets/benefits/3rd card.png',
-        CARD_4TH: '/assets/benefits/4th card.png',
-        CARD_5TH: '/assets/benefits/5th card.png',
+        CARD_1ST: '/assets/benefits/1st-card.png',
+        CARD_2ND: '/assets/benefits/2nd-card.png',
+        CARD_3RD: '/assets/benefits/3rd-card.png',
+        CARD_4TH: '/assets/benefits/4th-card.png',
+        CARD_5TH: '/assets/benefits/5th-card.png',
     },
 } as const;
 
